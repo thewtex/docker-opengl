8dbcd07e0db34d39ad8f79067282d4359d79439d
Matt McCormick <matt.mccormick@kitware.com>
2015-12-12 14:41:56 -0500

    Support an ARGS enviromental variable for extra command arguments.

--- usr/bin/graphical-app-launcher.py
+++ usr/bin/graphical-app-launcher.py
@@ -6,7 +6,13 @@ import subprocess
 if __name__ == '__main__':
     if os.environ.has_key('APP'):
         graphical_app = os.environ['APP']
-        process = subprocess.Popen(graphical_app, shell=True,
+        if os.environ.has_key('ARGS'):
+            extra_args = os.environ['ARGS']
+            command = graphical_app + ' ' + extra_args
+        else:
+            command = graphical_app
+
+        process = subprocess.Popen(command, shell=True,
                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdoutdata, stderrordata = process.communicate()
         print(stdoutdata)
